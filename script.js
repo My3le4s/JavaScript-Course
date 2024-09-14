a50da7ce23a48b76b3b73fbbff2ada1bd43e09f9
Sandra Mukwambo <125270129+My3le4s@users.noreply.github.com>
2024-09-14 17:27:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// This is my first javascript file
+// I've learnt of syntax rules in js
+// I am understanding variables and console.log
+
+let One = 45;
+console.log(One);
+
+var Two = 55;
+console.log(Two);
+
